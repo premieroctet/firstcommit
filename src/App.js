495287d900cb62edb3dae6ce3d69fb8f2b89a61e
Pierre <pierre.crbs@gmail.com>
2019-10-16 10:26:49 +0200

    fix header error & update design

--- src/App.js
+++ src/App.js
@@ -1,39 +1,87 @@
 import React, { useState } from "react";
-import throttle from 'lodash.throttle';
+import debounce from "lodash/debounce";
 import { parseLinkHeader } from "./utils";
+import Skeleton from "react-loading-skeleton";
 import client from "./api/client";
 
 import "./App.css";
 
 function App() {
-  const [ repositories,setRepositories ] = useState([]);
-  const [ firstCommit,setFirstCommit ] = useState();
+  const [repositories, setRepositories] = useState([]);
+  const [firstCommit, setFirstCommit] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const getFirstCommit = async (repository) => {
+  const getFirstCommit = async repository => {
+    setLoading(true);
     let response = await client.get(`/repos/${repository}/commits`);
-    const links = parseLinkHeader(response.headers.link);
-  
-    response = await client.get(links.last);
 
-    const lastCommit = response.data[ response.data.length - 1 ];
-    
-    setFirstCommit(lastCommit.html_url)
-  }
-  
-  const searchRepositories = throttle(async (term) => {
-    let response = await client.get(`/search/repositories?q=${term}&per_page=5`);
+    if (response.headers.link) {
+      const links = parseLinkHeader(response.headers.link);
+      response = await client.get(links.last);
+    }
+
+    const lastCommit = response.data[response.data.length - 1];
+
+    setFirstCommit(lastCommit.html_url);
+    setLoading(false);
+  };
+
+  const searchRepositories = debounce(async term => {
+    let response = await client.get(
+      `/search/repositories?q=${term}&per_page=5`
+    );
     const repositories = response.data.items;
-    setRepositories(repositories.map(repository => repository.full_name))
+    setRepositories(repositories.map(repository => repository.full_name));
   }, 500);
 
   return (
-      <div className="App">
-        <form>
-          <input onChange={e => searchRepositories(e.target.value)} type="text" />
-        </form>
-      {repositories.map(repository => <div onClick={() => getFirstCommit(repository)} key={repository}>{repository}</div>)}
-      {firstCommit && <div><a href={firstCommit}>{firstCommit}</a></div>}
-      </div>
+    <div className="App">
+      <p className="title">First Commit</p>
+      <p className="desc">
+        Pop up the first commit of any GitHub repo{" "}
+        <span role="img" aria-label="rocket">
+          🚀
+        </span>
+      </p>
+
+      <form>
+        <input
+          placeholder="Name of Github repository"
+          onChange={e => searchRepositories(e.target.value)}
+          type="text"
+        />
+      </form>
+      {repositories.map(repository => (
+        <div
+          className="suggestion"
+          onClick={() => getFirstCommit(repository)}
+          key={repository}
+        >
+          {repository}
+        </div>
+      ))}
+
+      {firstCommit && (
+        <div className="commit-container">
+          {loading ? (
+            <Skeleton />
+          ) : (
+            <a href={firstCommit} target="_blank" rel="noopener noreferrer">
+              <button className="commit-button">
+                See the first commit
+                <span
+                  style={{ marginLeft: "8px" }}
+                  role="img"
+                  aria-label="checkmark"
+                >
+                  ✅
+                </span>{" "}
+              </button>
+            </a>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,19 @@
-export function parseLinkHeader(header) {
-    if (header.length === 0) {
-        throw new Error("input must not be of zero length");
+export function parseLinkHeader(linkHeader) {
+  if (linkHeader.length === 0) {
+    throw new Error("input must not be of zero length");
+  }
+
+  // Split parts by comma and parse each part into a named link
+  return linkHeader.split(/(?!\B"[^"]*),(?![^"]*"\B)/).reduce((links, part) => {
+    const section = part.split(/(?!\B"[^"]*);(?![^"]*"\B)/);
+    if (section.length < 2) {
+      throw new Error("section could not be split on ';'");
     }
+    const url = section[0].replace(/<(.*)>/, "$1").trim();
+    const name = section[1].replace(/rel="(.*)"/, "$1").trim();
 
-    // Split parts by comma and parse each part into a named link
-    return header.split(/(?!\B"[^"]*),(?![^"]*"\B)/).reduce((links, part) => {
-        const section = part.split(/(?!\B"[^"]*);(?![^"]*"\B)/);
-        if (section.length < 2) {
-            throw new Error("section could not be split on ';'");
-        }
-        const url = section[0].replace(/<(.*)>/, '$1').trim();
-        const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+    links[name] = url;
 
-        links[name] = url;
-
-        return links;
-    }, {});
+    return links;
+  }, {});
 }
